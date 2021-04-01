2d42e4c35166d029c24c3a8fe156aad18bc9bdb0
Berkayuu <81733666+Berkayuu@users.noreply.github.com>
2021-04-01 14:18:25 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -20,18 +20,18 @@ async def help(event):
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
         else:
-            await event.edit("**Maaf Lord, Saya Tidak Punya Perintah Itu ツ**")
+            await event.edit("**ga ada itu goblok**")
             await asyncio.sleep(200)
             await event.delete()
     else:
-        await event.edit("⚡")
+        await event.edit("🪵")
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`\t✥  "
-        await event.edit("**❉ Daftar Perintah Untuk\nLord-Userbot:\n\n**"
+        await event.edit("**🪵 module of BERKAYU:\n\n**"
                          f"✥{string}✥"
-                         "\n✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗")
-        await event.reply(f"\n**Ketik Contoh** `.help afk` **Untuk Informasi Perintah**")
+                         "\n🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵")
+        await event.reply(f"\n**nih Contoh** `.help kontol` **Untuk Informasi Perintah**")
         await asyncio.sleep(1000)
         await event.delete()
