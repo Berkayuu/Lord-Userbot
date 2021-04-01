909185ef8221c39400e6eecbdccb53d3229080c5
Berkayuu <81733666+Berkayuu@users.noreply.github.com>
2021-04-01 12:46:00 +0700

    Update oi.py

--- userbot/modules/oi.py
+++ userbot/modules/oi.py
@@ -2,37 +2,57 @@ from time import sleep
 from userbot.events import register
 
 
-@register(outgoing=True, pattern='^.alvin(?: |$)(.*)')
+@register(outgoing=True, pattern='^.cicak(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(3)
-    await typew.edit("`Hai Perkenalkan Namaku Alvin`")
-    sleep(3)
-    await typew.edit("`17 Tahun`")
+    sleep(2)
+    await typew.edit("`HAYO GAYS NAMAKU UCHIHA ENDI`")
+    sleep(2)
+    await typew.edit("`16 ᴛᴀʜᴜɴ`")
     sleep(1)
-    await typew.edit("`Tinggal Dibatam, Salam Kenal:)`")
+    await typew.edit("`ᴀᴋᴜ ᴛɪɴɢɢᴀʟ ᴅɪ ʙᴀɴᴅᴜɴɢ, ꜱᴀʟᴀᴍ ᴋᴇɴᴀʟ:)`")
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern='^.sayang(?: |$)(.*)')
+@register(outgoing=True, pattern='^.ayang(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(3)
-    await typew.edit("`Cuma Mau Bilang`")
+    await typew.edit("`Hai Bulan Aku Mau Bilang`")
     sleep(3)
-    await typew.edit("`Aku Sayang Kamu`")
+    await typew.edit("`Aku Sayang Kamu Bulannn`")
     sleep(1)
-    await typew.edit("`I LOVE YOU 💞`")
+    await typew.edit("`I LOVE YOU MWAHH 💞`")
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern='^.semangat(?: |$)(.*)')
+@register(outgoing=True, pattern='^.ssadt(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(3)
-    await typew.edit("`Apapun Yang Terjadi`")
-    sleep(3)
-    await typew.edit("`Tetaplah Bernapas`")
+    sleep(2)
+    await typew.edit("`𝘏𝘌𝘠 𝘒𝘈𝘔𝘜 𝘠A𝘕𝘎 𝘓𝘈𝘎𝘐 𝘚𝘌𝘋𝘈𝘕𝘎 𝘚𝘌𝘋𝘐𝘏 `")
+    sleep(2)
+    await typew.edit("`𝘈𝘒𝘜 𝘊𝘜𝘔𝘈 𝘔𝘈𝘜 𝘉𝘐𝘓𝘈𝘕𝘎`")
+    sleep(2)
+    await typew.edit("`𝘈𝘓𝘈𝘠 𝘓𝘜 𝘕𝘎𝘌𝘕𝘛𝘖𝘛 𝘚𝘖 𝘚𝘖𝘈𝘕 𝘚𝘌𝘋𝘐𝘏`")
+    sleep(2)
+    await typew.edit("`𝘔𝘌𝘕𝘋𝘐𝘕𝘎 𝘔𝘈𝘛𝘐 𝘈𝘑𝘈 𝘛𝘖𝘓𝘖𝘓𝘓`")
     sleep(1)
-    await typew.edit("`Dan Selalu Bersyukur`")
+    await typew.edit("`𝘉𝘜𝘈𝘛 𝘒𝘈𝘔𝘜 𝘐 𝘓𝘖𝘝𝘌 𝘜𝘜𝘜𝘜𝘜`")
+# Create by myself @localheart
+
+
+@register(outgoing=True, pattern='^.piw(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("`kiw cewe`")
+    sleep(2)
+    await typew.edit("`kamu tau ga")
+    sleep(2)
+    await typew.edit("`ada yang lebih indah dari bintang`")
+    sleep(2)
+    await typew.edit("`yaitu indomie + nasi`")
+    sleep(1)
+    await typew.edit("`kamu mau ga jadi pacar aku?`")
 # Create by myself @localheart
