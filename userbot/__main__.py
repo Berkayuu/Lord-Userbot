69474644ab9be4d4db0adc7dea7baabfc2af9446
Berkayuu <81733666+Berkayuu@users.noreply.github.com>
2021-04-01 12:31:24 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"Lord-Userbot ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"KAYU TELAH RAPUH ⚙️ V{BOT_VER} [SEPERTI HATIMU!]")
 
 
 if len(argv) not in (1, 3, 4):
